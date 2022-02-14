4cda39b603dfc7e4b75e13be77e6f78c99ae2e1e
marblig <marcusblight@hotmail.com>
2022-02-14 09:19:39 +0000

    heroku commit

---
+++ build/precache-manifest.df45d88e1553cfa886d2b4799c9492d8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d3f6649234265253b794834b54910d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d06907b267746c6e3f1",
+    "url": "/static/css/main.caa60ff0.chunk.css"
+  },
+  {
+    "revision": "1bb1a865c594016d0f2f",
+    "url": "/static/js/2.7af4898a.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/static/js/2.7af4898a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d06907b267746c6e3f1",
+    "url": "/static/js/main.f8db2fa0.chunk.js"
+  },
+  {
+    "revision": "63d96dfdde8599f4b7f5",
+    "url": "/static/js/runtime-main.b37db9a5.js"
+  },
+  {
+    "revision": "970b6d3d7ba5c2321d7055d36da2734c",
+    "url": "/static/media/b_w.970b6d3d.png"
+  },
+  {
+    "revision": "1c44bc51045fcc7e279abb9614497fba",
+    "url": "/static/media/k_b.1c44bc51.png"
+  },
+  {
+    "revision": "23d4d33c170190be90b6648256588687",
+    "url": "/static/media/k_w.23d4d33c.png"
+  },
+  {
+    "revision": "0729b04db617bb3d592452f5354a1e0d",
+    "url": "/static/media/n_b.0729b04d.png"
+  },
+  {
+    "revision": "34a63331ca2d197798cdc29460801ea1",
+    "url": "/static/media/n_w.34a63331.png"
+  },
+  {
+    "revision": "c7a447c234fad5eb8ae844506186ebc0",
+    "url": "/static/media/p_w.c7a447c2.png"
+  },
+  {
+    "revision": "1fee21b2c744d202f8e59f919591a2fd",
+    "url": "/static/media/q_b.1fee21b2.png"
+  },
+  {
+    "revision": "f511b320fd4c861a8fd74b015867a13a",
+    "url": "/static/media/q_w.f511b320.png"
+  },
+  {
+    "revision": "80d296d4a3ba0bbabc413fa71995bbf4",
+    "url": "/static/media/r_w.80d296d4.png"
+  }
+]);
